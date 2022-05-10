1374800498a74aec023e6c9857ff2ed74940fda4
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-10 22:34:31 +0300

    ft: create views for pitches

--- app/pitch/forms.py
+++ app/pitch/forms.py
@@ -7,5 +7,5 @@ from wtforms import ValidationError
 CATEGORY_CHOICES = [('pickup_lines', 'pickup-lines'), ('interview', 'interview'), ('promotion', 'promotion'), ('victory', 'victory'), ('motivation', 'motivation')]
 
 class submitPitch(FlaskForm):
-    category = SelectField(u'Choose a category for your pitch', choices=CATEGORY_CHOICES)
+    category = SelectField(u'Choose a category for your pitch', choices=CATEGORY_CHOICES, validators = [DataRequired()])
     submit = SubmitField('Submit Pitch')
--- app/pitch/views.py
+++ app/pitch/views.py
@@ -7,6 +7,11 @@ from . import pitch
 @pitch.route('/pitch')
 def pitch():
 
-    pitch_form = submitPitch()
+    pitches = Pitch.query.all()
+    pickup_lines = Pitch.query.filter_by(category = 'pickup-lines').all()
+    promotion = Pitch.query.filter_by(category = 'promotion').all()
+    victory = Pitch.query.filter_by(category = 'victory').all()
+    motivation = Pitch.query.filter_by(category = 'motivation').all()
 
-    return render_template('pitch/pitch.html', pitch_form = pitch_form)
+
+    return render_template('pitch/pitch.html', pitches = pitches, pickup_lines = pickup_lines, promotion = promotion, victory = victory, motivation = motivation)
